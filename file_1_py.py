7b3ed9ac26d56e55262defd78c71314d8a32fad9
UpendraLe355 <162982249+UpendraLe355@users.noreply.github.com>
2024-03-11 16:14:26 +0530

    Add files via upload

---
+++ file_1_py.py
@@ -0,0 +1,89 @@
+'''
+THEORY
+day 1(11/03)
+--->(1) Bargained Characters
+1.controls and data structures-->ABC language
+2.String and its formats-->from C
+3.Regular expression-->pearl
+4.Exception handling-->Module 3
+5.Magic methods from-->fortron
+
+--->(2) WHAT KIND OF LANGUAGE IS PYTHON
+Python is a
+> Object oriented programing language
+> Dynamically type language
+> Scripting language
+> Interpreted language-> Identify by it self.
+> Compiled language
+> Funtional programing language
+
+--->(3) FLAVORS OF PYTHON
+> Cpython- Written in C, Most common implementation of python.
+> Jython- Java implemantation of python.
+> Ironppython- Implementation of the python programming language which is tightly inttegrated with.NET.
+> brython- Browser python, runs in browser
+> Rubypython- Bridge between python and Ruby interpreter
+> Micropython- Runs in micro controllers
+
+--->(4) # print(object, sep, end, file, flush)
+#Print has 5 perameters
+#object --> the value to be printed is object
+# sep--> seperater
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a, b, c, sep='$')
+# end--> which is used to add the string at the end of the value,
+# it will not keep the command to stay in same line
+#eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+--->(5) VARIABLES
+# ?Variable is name which is used to identify the value of variable
+# ?which is stored in a specific memory location
+
+#--->(6) RULES TO DEDFINE VARIABLES
+# 1) Variable have to start with only alphabets, underscore
+# 2) The char following the 1st char can be alphabets,numbers,underscores.
+# 3) No spaces are allowed
+# 4) No special characecters like !@#$%^&*()+
+# 5) Case sensitive
+# 6)Keywords are not allowed as variables
+
+CODE---->
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+---> commenting
+1.)commenting is used for making documentation in a python file
+2.)when the line is commented, that line will not be considerd
+
+#print("hello world")
+
+import this
+print(this)
+from file_1 import a, b
+# import is a keyword which is used to communicated between the python files.
+
+#import antigravity
+
+# ! ways to define variables
+
+ a = 7
+ b = 7
+
+ a = b =7
+print(type(a))
+'''
+
+
+
+        
+
+
